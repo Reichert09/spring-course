4a07374728c6d1ba67399c44fb70e9af6b49b37f
Lucas Reichert <lucasreichert3@gmail.com>
2019-07-24 20:58:26 -0300

    Implementing JPA and createdTables

--- src/main/java/com/examplecourse/SpringCourseApplication.java
+++ src/main/java/com/examplecourse/SpringCourseApplication.java
@@ -2,8 +2,10 @@ package com.examplecourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.springcourse.domain"})
 public class SpringCourseApplication {
 
 	public static void main(String[] args) {
